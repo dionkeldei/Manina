5e9c9e2c946f78f60b2dc5d2c453402283f19d26
Dionkeldei <dionkeldei@gmail.com>
2021-02-23 11:32:15 -0600

    post and get with url parameters

--- framework/routes/route.js
+++ framework/routes/route.js
@@ -39,22 +39,11 @@ class route {
   }
 
   get(route,action){
-    var valuePositions = this.getVariablePositions(route);
     if(!this.isFunction(action)){
       action = this.getControllerFunction(action)
     }
     this.app.get(route, (req, res) => {
-      var values = []
-      if(valuePositions){
-         var routeArray = this.getVariableValues(route);
-         for(var i in valuePositions){
-           var value = routeArray[valuePositions[i]].split(':')[1]
-           values.push(req.params[value])
-         }
-        res.send(this.constructFunctionWithParams(action,values))
-      }else{
-        res.send(action())
-      }
+        res.send(this.constructFunctionWithParams(req,action,route))
     })
   }
 
@@ -63,7 +52,7 @@ class route {
       action = this.getControllerFunction(action)
     }
     this.app.post(route, (req, res) => {
-      res.send(action(req.body))
+      res.send(this.constructFunctionWithParams(req,action,route,true))
     })
   }
 
@@ -100,34 +89,65 @@ class route {
     return variables;
   }
 
-  constructFunctionWithParams(action,values){
-
+  constructFunctionWithParams(req,action,route,post=false){
+    var valuePositions = this.getVariablePositions(route);
+    var values = []
+    if(valuePositions){
+       var routeArray = this.getVariableValues(route);
+       for(var i in valuePositions){
+         var value = routeArray[valuePositions[i]].split(':')[1]
+         values.push(req.params[value])
+       }
+    }else{
+      return action()
+    }
     switch(values.length){
-      case 0:
-        return action()
-      break;
       case 1:
+        if(post){
+          return action(req.body,values[0])
+        }
         return action(values[0])
       break;
       case 2:
+        if(post){
+          return action(req.body,values[0],values[1])
+        }
         return action(values[0],values[1])
       break;
       case 3:
+        if(post){
+          return action(req.body,values[0],values[1],values[2])
+        }
         return action(values[0],values[1],values[2])
       break;
       case 4:
+        if(post){
+          return action(req.body,values[0],values[1],values[2],values[3])
+        }
         return action(values[0],values[1],values[2],values[3])
       break;
       case 5:
+        if(post){
+          return action(req.body,values[0],values[1],values[2],values[3],values[4])
+        }
         return action(values[0],values[1],values[2],values[3],values[4])
       break;
       case 6:
+        if(post){
+          return action(req.body,values[0],values[1],values[2],values[3],values[4],values[5])
+        }
         return action(values[0],values[1],values[2],values[3],values[4],values[5])
       break;
       case 7:
+        if(post){
+        return action(req.body,values[0],values[1],values[2],values[3],values[4],values[5],values[6])
+        }
         return action(values[0],values[1],values[2],values[3],values[4],values[5],values[6])
       break;
       case 8:
+        if(post){
+          return action(req.body,values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7])
+        }
         return action(values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7])
       break;
       default:
